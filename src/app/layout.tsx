bc8acfe9240473a21cd491f5c50aa36604ed1c3c
Zach Yeo <zyeo@users.noreply.github.com>
2026-05-22 01:32:38 +0900

    Add smooth scroll behavior marker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <body className={`${display.variable} ${serif.variable} ${signature.variable} ${sans.variable}`}>
         {children}
       </body>
